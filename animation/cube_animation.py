b001cba02ece48c54f23be77169b80cdc21b521d
Tyler McKay <tmckay1.jackson@gmail.com>
2020-10-20 23:50:23 -0400

    create cube anim

--- animation/cube_animation.py
+++ animation/cube_animation.py
@@ -3,9 +3,12 @@ from bibliopixel.animation import Animation
 
 class CubeAnimation(Animation):
 
+    _led = None
+
     def __init__(self, led):
         #The base class MUST be initialized by calling super like this
-        super(Animation, self).__init__(led)
+        super(Animation, self).__init__()
+        self._led = led
 
     # override to write out word for each frame in the animation
     def step(self, amt = 1):
