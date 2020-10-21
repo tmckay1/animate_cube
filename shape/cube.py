2b0be56458569216d78744d1f2aa3453f37a0b85
Tyler McKay <tmckay1.jackson@gmail.com>
2020-10-20 23:59:00 -0400

    create cube anim

--- shape/cube.py
+++ shape/cube.py
@@ -2,8 +2,47 @@ from bibliopixel.util import log
 from bibliopixel.colors import conversions
 from bibliopixel.layout import font
 from bibliopixel.layout.layout import Layout
-from bibliopixel.layout.geometry.cube import make_cube_coord_map, make_cube_coord_map_positions
+# from bibliopixel.layout.geometry.cube import make_cube_coord_map, make_cube_coord_map_positions
+from bibliopixel.layout.geometry.rotation import rotate_and_flip
+from bibliopixel.layout.geometry.matrix import make_matrix_coord_map
 import math, threading, time
+
+
+def make_cube_coord_map(dx, dy, dz, xy_serpentine=True, offset=0,
+                        xy_rotation=0, z_rotation=0,
+                        y_flip=False, z_flip=False):
+    result = []
+    plane_offset = offset
+    for z in range(dz):
+        plane = make_matrix_coord_map(dx, dy, serpentine=xy_serpentine,
+                                      offset=plane_offset, rotation=xy_rotation,
+                                      y_flip=y_flip)
+        plane_offset += (dx * dy)
+        result.append(plane)
+
+    result = rotate_and_flip(result, z_rotation, z_flip)
+
+    return result
+
+
+def make_cube_coord_map_positions(coord_map):
+    num = 0
+    dx, dy, dz = (0, 0, len(coord_map))
+
+    for xy in coord_map:
+        if len(xy) > dx:
+            dy = len(xy)
+        for x in xy:
+            if len(x) > dx:
+                dx = len(x)
+            num += len(x)
+
+    result = [None] * num
+    for z in range(dz):
+        for y in range(dy):
+            for x in range(dx):
+                result[coord_map[z][y][x]] = [x, y, z]
+    return result
 
 class Cube(Layout):
     CLONE_ATTRS = Layout.CLONE_ATTRS + ('x', 'y', 'z')
