06092083fa17009741290d756e1828bd383bcecd
Tyler McKay <tmckay1.jackson@gmail.com>
2020-10-21 21:24:33 -0400

    fix snaked cube

--- shape/snaked_cube.py
+++ shape/snaked_cube.py
@@ -3,6 +3,7 @@ from bibliopixel.colors import conversions
 from bibliopixel.layout import font
 from bibliopixel.layout.cube import Cube
 from bibliopixel.layout.geometry.rotation import rotate_and_flip
+from bibliopixel.layout.geometry.cube import make_cube_coord_map_positions
 from bibliopixel.layout.geometry.matrix import make_matrix_coord_map
 import math, threading, time
 
@@ -23,74 +24,8 @@ def make_cube_coord_map(dx, dy, dz, xy_serpentine=True, offset=0,
 
     return result
 
-
-def make_cube_coord_map_positions(coord_map):
-    num = 0
-    dx, dy, dz = (0, 0, len(coord_map))
-
-    for xy in coord_map:
-        if len(xy) > dx:
-            dy = len(xy)
-        for x in xy:
-            if len(x) > dx:
-                dx = len(x)
-            num += len(x)
-
-    result = [None] * num
-    for z in range(dz):
-        for y in range(dy):
-            for x in range(dx):
-                result[coord_map[z][y][x]] = [x, y, z]
-    return result
-
 class SnakedCube(Cube):
-    def __init__(self, drivers, x, y, z, coord_map=None,
-                 threadedUpdate=False, brightness=255, **kwargs):
-        super().__init__(drivers, threadedUpdate, brightness, **kwargs)
-        self.x = x
-        self.y = y
-        self.z = z
-
-        if self.x * self.y * self.z != self.numLEDs:
-            raise TypeError("(x * y * z) MUST equal the total pixel count!")
-
-        if coord_map:
-            self.coord_map = coord_map
-        else:
-            if len(self.drivers) == 1:
-                self.coord_map = make_cube_coord_map(x, y, z)
-            else:
-                raise TypeError(
-                    "Must provide coord_map if using multiple drivers!")
-
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.coord_map = make_cube_coord_map(x, y, z)
         self.set_pixel_positions(make_cube_coord_map_positions(self.coord_map))
-
-    def get_pixel_positions(self):
-        return make_cube_coord_map_positions(self.coord_map)
-
-    @property
-    def shape(self):
-        """Returns ``x, y, z``"""
-        return self.x, self.y, self.z
-
-    def set(self, x, y, z, color):
-        try:
-            pixel = self.coord_map[z][y][x]
-            self._set_base(pixel, color)
-        except IndexError:
-            pass
-
-    def get(self, x, y, z):
-        try:
-            pixel = self.coord_map[z][y][x]
-            return self._get_base(pixel)
-        except IndexError:
-            return 0, 0, 0
-
-    def setHSV(self, x, y, z, hsv):
-        color = conversions.hsv2rgb(hsv)
-        self._set(x, y, z, color)
-
-    def setRGB(self, x, y, z, r, g, b):
-        color = (r, g, b)
-        self._set(x, y, z, color)
