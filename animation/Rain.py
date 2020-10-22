6aab9e3115a82c8c1773a8169db8ac0c11812af3
Tyler McKay <tmckay1.jackson@gmail.com>
2020-10-21 21:29:44 -0400

    make rain go z direction

--- animation/Rain.py
+++ animation/Rain.py
@@ -7,7 +7,7 @@ class RainBow(Cube):
   def __init__(self, layout, tail=4, growthRate=12, **kwds):
     super().__init__(layout, **kwds)
     self._tail = tail
-    self._drops = [[[] for z in range(self.z)] for x in range(self.x)]
+    self._drops = [[[] for y in range(self.y)] for x in range(self.x)]
     self._growthRate = growthRate
 
   def pre_run(self):
@@ -15,34 +15,34 @@ class RainBow(Cube):
 
   def _drawDrop(self, x, y, z, color):
     for i in range(self._tail):
-      if y - i >= 0 and y - i < self.y:
+      if z - i >= 0 and z - i < self.z:
         level = 255 - ((255 // self._tail) * i)
-        self.layout.set(x, y - i, z, color_scale(color, level))
+        self.layout.set(x, y, z - i, color_scale(color, level))
 
   def step(self, amt=1):
     self.layout.all_off()
 
     for i in range(self._growthRate):
       x = random.randint(0, self.x - 1)
-      z = random.randint(0, self.z - 1)
-      self._drops[x][z].append(0)
+      y = random.randint(0, self.y - 1)
+      self._drops[x][y].append(0)
 
     for x in range(self.x):
-      for z in range(self.z):
-        col = self._drops[x][z]
+      for y in range(self.y):
+        col = self._drops[x][y]
         if len(col) > 0:
           removals = []
           for y in range(len(col)):
             drop = col[y]
             if drop < self.y:
-              self._drawDrop(x, drop, z, self.palette(
+              self._drawDrop(x, y, drop, self.palette(
                 drop * (255 // self.y)))
             if drop - (self._tail - 1) < self.y:
               drop = drop + 1
-              self._drops[x][z][y] = drop
+              self._drops[x][y][z] = drop
             else:
               removals.append(drop)
           for r in removals:
-            self._drops[x][z].remove(r)
+            self._drops[x][y].remove(r)
 
     self._step = 0
