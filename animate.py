545fe1d4ededfd975a5e25386c01c5f22f96c5a4
Tyler McKay <tmckay1.jackson@gmail.com>
2020-10-23 20:03:33 -0400

    add x y z

--- animate.py
+++ animate.py
@@ -15,16 +15,16 @@ brightness = 100     # brightness 0-255
 x          = 8    # number of leds in the team name
 y          = 8    # number of leds in the timer section
 z          = 8     # number of leds in for a single number
-driver     = PiWS281X(8*8*8)
-layout     = SnakedCube(driver, 8, 8, 8)
+driver     = PiWS281X(x*y*z)
+layout     = SnakedCube(driver, x, y, z)
 
 # matrix movie animation
-# tail       = 4 # tail of rain animation
-# growthRate = 1 # how many rain drops
-# speed      = 2 # higher the number, slower it is
-# color      = (0,255,0)
-# anim       = CubeRain(layout, tail=tail, growthRate=growthRate, speed=speed, color=color)
-# anim.run()
+tail       = 4 # tail of rain animation
+growthRate = 1 # how many rain drops
+speed      = 2 # higher the number, slower it is
+color      = (0,255,0)
+anim       = CubeRain(layout, tail=tail, growthRate=growthRate, speed=speed, color=color)
+anim.run()
 
 # rain drops
 # tail       = 1 # tail of rain animation
@@ -39,9 +39,9 @@ layout     = SnakedCube(driver, 8, 8, 8)
 # anim.run()
 
 # laser
-speed = 5 # higher the number, slower it is
-anim  = Laser(layout, speed=speed)
-anim.run()
+# speed = 5 # higher the number, slower it is
+# anim  = Laser(layout, speed=speed)
+# anim.run()
 
 # laser up
 # speed = 2 # higher the number, slower it is
