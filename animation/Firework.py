308d5bfc2f8b3f405c4660e6d3310963fa18dfd5
Tyler McKay <tmckay1.jackson@gmail.com>
2020-10-23 23:21:41 -0400

    fix firework

--- animation/Firework.py
+++ animation/Firework.py
@@ -42,14 +42,14 @@ class Firework(Cube):
             new_step = (self._step // self._speed)
 
             # draw a line for the first third of the animation, then the circle for the rest
-            full_cycle_length = (3 * self.z // 2)
+            full_cycle_length = (3 * self.z // 2) # 12
             cycle_step = new_step % full_cycle_length
-            line_cycle_length = (self.z // 2)
+            line_cycle_length = (self.z // 2) # 4
             draw_circle = cycle_step >= line_cycle_length
 
             if draw_circle:
                 # this respects master brightness but is slower
-                radius = (new_step - line_cycle_length) % self.z
+                radius = (cycle_step - line_cycle_length) % self.z
                 for z in range(self.z):
                     for y in range(self.y):
                         for x in range(self.x):
